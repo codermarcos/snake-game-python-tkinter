56d10fb88cd9b8dc4f856e496f13a19e23b58a7d
codermarcos <coder.marcos@gmail.com>
2024-05-01 03:17:06 -0300

    🆕 create food class and constants file to reuse values On branch main Changes to be committed: 	new file:   constants.py 	new file:   food.py

---
+++ food.py
@@ -0,0 +1,29 @@
+from tkinter import Canvas
+
+from numpy import random
+
+from constants import GAME_SPACE_SIZE, GAME_SPACES
+
+
+class Food:
+
+    def __init__(self, canvas: Canvas):
+        self.canvas_tag = "Food"
+        self.canvas = canvas
+        self.place()
+
+    def generate_position(self) -> int:
+        return random.randint(0, GAME_SPACES - 1) * GAME_SPACE_SIZE
+
+    def place(self):
+        x = self.generate_position()
+        y = self.generate_position()
+        self.position = [x, y]
+        self.canvas.create_oval(
+            x,
+            y,
+            x + GAME_SPACE_SIZE,
+            y + GAME_SPACE_SIZE,
+            fill="#ff0",
+            tags=self.canvas_tag,
+        )
