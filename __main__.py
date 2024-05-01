e95950c8e1c5fe0aca81a9c391bf8f4be431048d
codermarcos <coder.marcos@gmail.com>
2024-05-01 03:15:16 -0300

    🎨 move game class to a different file On branch main Changes to be committed: 	modified:   __main__.py 	new file:   game.py

--- __main__.py
+++ __main__.py
@@ -1,29 +1,3 @@
-from tkinter import *
-
-
-class Game(Tk):
-
-    def __init__(self):
-        Tk.__init__(self)
-
-        self.score = 0
-
-        self.setup_screen()
-
-        self.mainloop()
-
-    def setup_screen(self):
-        self.title("Jogo da cobrinha")
-        self.iconbitmap(default="snake.ico")
-
-        self.resizable(False, False)
-        self.geometry("{}x{}".format(500, 500 + 70))
-
-        self.score_label = Label(self, text="{}".format(self.score), font=("arial", 40))
-        self.score_label.pack()
-
-        self.canvas = Canvas(self, bg="#000", width=500, height=500)
-        self.canvas.pack()
-
+from game import *
 
 Game()
